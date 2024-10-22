5510f9bed16c5c2fa722b241f1f15bd86482f1a3
MajdDev23 <124749255+MajdDev23@users.noreply.github.com>
2024-10-22 09:21:46 -0300

    Added some cam stuff
    I added some of the code for the camera from lab 3, for some reason my app won't run because it needs to sync with gradle and I can't find a way to do that. So if you guys can try to run it and see if t works (or crashes) that would be great.

--- PlantPal2/app/src/main/java/ca/unb/mobiledev/plantpal/CameraActivity.kt
+++ PlantPal2/app/src/main/java/ca/unb/mobiledev/plantpal/CameraActivity.kt
@@ -5,8 +5,55 @@ import android.os.Bundle
 class CameraActivity {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.camera_btn)
 
+
+                // Register the activity listener
+                setCameraActivityResultLauncher()
+
+                val camButton = findViewById<Button>(R.id.camButton)
+
+                camButton.setOnClickListener {
+                    dispatchTakePictureIntent()
+                }
+        }
+
     }
+
+
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    cameraActivityResultLauncher!!.launch(takePictureIntent)
+                }
+            }
+        }
+    private fun setCameraActivityResultLauncher() {
+        // Handle the image capture result
+        // TODO - Implement later
+        cameraActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result: ActivityResult ->
+            if (result.resultCode == RESULT_OK) {
+                galleryAddPic()
+            }
+        }
+
+    }
+
 }
+
